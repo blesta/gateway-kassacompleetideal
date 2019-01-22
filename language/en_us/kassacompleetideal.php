32589c21a95eaa76cf39ce34a6ec55297180e94a
Temporary <Temporary@Paul-XPS>
2019-01-21 16:23:20 -0800

    initial commit

---
+++ language/en_us/kassacompleetideal.php
@@ -0,0 +1,10 @@
+<?php
+$lang['Kassacompleetideal.!error.api_key.empty'] = 'Please enter an API key';
+
+$lang['Kassacompleetideal.name'] = 'iDeal (Kassa Compleet)';
+
+$lang['Kassacompleetideal.api_key'] = 'API Key';
+$lang['Kassacompleetideal.tooltip.api_key'] = 'In order to use test mode, simply enter an key from a test account';
+
+$lang['Kassacompleetideal.buildprocess.issuer_id'] = 'iDeal Issuer';
+$lang['Kassacompleetideal.buildprocess.submit'] = 'Pay with iDeal via Kassa Compleet';
