f3d3e2c68a336226ea7da176b7cdf7fd74a14e42
Temporary <Temporary@Paul-XPS>
2020-01-22 15:31:08 -0800

    Updated readme, updated config, updated description

--- language/en_us/kassacompleetideal.php
+++ language/en_us/kassacompleetideal.php
@@ -3,6 +3,7 @@ $lang['Kassacompleetideal.!error.api_key.empty'] = 'Please enter an API key';
 $lang['Kassacompleetideal.!error.api_key.valid'] = 'API key is invalid';
 
 $lang['Kassacompleetideal.name'] = 'iDeal (Kassa Compleet)';
+$lang['Kassacompleetideal.description'] = 'Kassa Compleet integration using the iDeal payment type';
 
 $lang['Kassacompleetideal.api_key'] = 'API Key';
 $lang['Kassacompleetideal.tooltip.api_key'] = 'In order to use test mode, simply enter an API key from a test account';
